552c1839737995f5dd36714ad4b6d9b46f202afd
Gerardo Hernandez <edgewl2@gmail.com>
2024-02-09 22:18:28 -0600

    feat(models): add required validation fields

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
   belongs_to :source
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,4 @@
 class Source < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
   has_many :articles
 end
